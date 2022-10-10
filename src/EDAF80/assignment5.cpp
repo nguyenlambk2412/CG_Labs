da3c3b7d642ecb0e15f81212ab50c6768b5b6ad2
nguyenlambk2412 <nguyenlambk2412@outlook.com>
2022-10-10 21:56:54 +0200

    A5_GameState_ShipSphere

--- src/EDAF80/assignment5.cpp
+++ src/EDAF80/assignment5.cpp
@@ -19,7 +19,6 @@
 #include <stdlib.h>
 
 
-
 edaf80::Assignment5::Assignment5(WindowManager& windowManager) :
 	mCamera(0.5f * glm::half_pi<float>(),
 	        static_cast<float>(config::resolution_x) / static_cast<float>(config::resolution_y),
@@ -44,6 +43,11 @@ edaf80::Assignment5::~Assignment5()
 void
 edaf80::Assignment5::run()
 {
+	enum class GameState {
+		GAME_START,
+		GAME_RUN,
+		GAME_FINISH
+	};
 	// Set up the camera
 	mCamera.mWorld.SetTranslate(glm::vec3(0.0f, 0.5f, 1.3f));
 	mCamera.mMouseSensitivity = glm::vec2(0.003f);
@@ -53,6 +57,17 @@ edaf80::Assignment5::run()
 
 	// Create the shader programs
 	ShaderProgramManager program_manager;
+	//diffuse
+	GLuint spaceshipSphere_shader = 0u;
+	program_manager.CreateAndRegisterProgram("spaceshipSphere",
+		{ { ShaderType::vertex, "EDAF80/A5_spaceshipSphere.vert" },
+		  { ShaderType::fragment, "EDAF80/A5_spaceshipSphere.frag" } },
+		spaceshipSphere_shader);
+	if (spaceshipSphere_shader == 0u) {
+		LogError("Failed to load spaceshipSphere_shader");
+		return;
+	}
+
 	//skybox shader
 	GLuint skybox_shader = 0u;
 	program_manager.CreateAndRegisterProgram("skybox",
@@ -139,6 +154,14 @@ edaf80::Assignment5::run()
 		return;
 	}
 
+	float spaceshipRadius = 3.0f;
+	// create the spaceship sphere shape
+	auto spaceship_Sphere = parametric_shapes::createSphere(spaceshipRadius, 10u, 10u);
+	if (spaceship_Sphere.vao == 0u) {
+		LogError("Failed to retrieve the mesh for the spaceship_Sphere");
+		return;
+	}
+
 	// create the spaceship
 	std::vector<bonobo::mesh_data> const spaceShipObj = bonobo::loadObjects(config::resources_path("ObjectModel/spaceship/source/Ship.obj"));
 	if (spaceShipObj.empty()) {
@@ -164,7 +187,6 @@ edaf80::Assignment5::run()
 
 	//SpaceShip
 	Node spaceship;
-	
 	spaceship.set_program(&spaceship_shader, phong_set_uniforms);
 	spaceship.add_texture("albedoEngTex", Albedo_Engine, GL_TEXTURE_2D);
 	spaceship.add_texture("ambientEngTex", AmbientOclussion_Engine, GL_TEXTURE_2D);
@@ -181,7 +203,10 @@ edaf80::Assignment5::run()
 	spaceship.add_texture("emissBodyTex", Emissive_Ship, GL_TEXTURE_2D);
 	spaceship.add_texture("maskEngTex", Mask_Engine, GL_TEXTURE_2D);
 
-	
+	Node spaceshipSphere;
+	spaceshipSphere.set_geometry(spaceship_Sphere);
+	spaceshipSphere.set_program(&spaceshipSphere_shader);
+
 	///Asteroid
 	//Asteroid location for the space ship
 	float asteroid_acceleration = 10.0f;
@@ -247,6 +272,8 @@ edaf80::Assignment5::run()
 	float basis_length_scale = 1.0f;
 	bool pause_bool = true;
 
+	GameState gameState = GameState::GAME_START;
+
 	while (!glfwWindowShouldClose(window)) {
 		auto const nowTime = std::chrono::high_resolution_clock::now();
 		auto const deltaTimeUs = std::chrono::duration_cast<std::chrono::microseconds>(nowTime - lastTime);
@@ -257,104 +284,103 @@ edaf80::Assignment5::run()
 
 		glfwPollEvents();
 		inputHandler.Advance();
-		//mCamera.Update(deltaTimeUs, inputHandler);
 
-		if (inputHandler.GetKeycodeState(GLFW_KEY_R) & JUST_PRESSED) {
-			shader_reload_failed = !program_manager.ReloadAllPrograms();
-			if (shader_reload_failed)
-				tinyfd_notifyPopup("Shader Program Reload Error",
-				                   "An error occurred while reloading shader programs; see the logs for details.\n"
-				                   "Rendering is suspended until the issue is solved. Once fixed, just reload the shaders again.",
-				                   "error");
-		}
-		if (inputHandler.GetKeycodeState(GLFW_KEY_F3) & JUST_RELEASED)
-			show_logs = !show_logs;
-		if (inputHandler.GetKeycodeState(GLFW_KEY_F2) & JUST_RELEASED)
-			show_gui = !show_gui;
-		if (inputHandler.GetKeycodeState(GLFW_KEY_F11) & JUST_RELEASED)
-			mWindowManager.ToggleFullscreenStatusForWindow(window);
-
-		//Control the spaceship
-		if (inputHandler.GetKeycodeState(GLFW_KEY_A) & PRESSED) {
-			spaceshipUpdate = true;
-			spaceshipLocation = -spaceshipHorSpeed;
-
-		}
-
-		if (inputHandler.GetKeycodeState(GLFW_KEY_D) & PRESSED) {
-			spaceshipUpdate = true;
-			spaceshipLocation = spaceshipHorSpeed;
-		}
-
-		if (inputHandler.GetKeycodeState(GLFW_KEY_W) & PRESSED) {
-			spaceshipVerSpeed = 80.0f;
-		}
-
-		if (inputHandler.GetKeycodeState(GLFW_KEY_S) & PRESSED) {
-			spaceshipVerSpeed = -20.0f;
-		}
-
-		if (inputHandler.GetKeycodeState(GLFW_KEY_W) & JUST_RELEASED) {
-			spaceshipVerSpeed = 0.0f;
-		}
-
-		if (inputHandler.GetKeycodeState(GLFW_KEY_S) & JUST_RELEASED) {
-			spaceshipVerSpeed = 0.0f;
-		}
-		// Retrieve the actual framebuffer size: for HiDPI monitors,
-		// you might end up with a framebuffer larger than what you
-		// actually asked for. For example, if you ask for a 1920x1080
-		// framebuffer, you might get a 3840x2160 one instead.
-		// Also it might change as the user drags the window between
-		// monitors with different DPIs, or if the fullscreen status is
-		// being toggled.
 		int framebuffer_width, framebuffer_height;
 		glfwGetFramebufferSize(window, &framebuffer_width, &framebuffer_height);
 		glViewport(0, 0, framebuffer_width, framebuffer_height);
-
-
-		//
-		// Todo: If you need to handle inputs, you can do it here
-		//
-
 
 		mWindowManager.NewImGuiFrame();
 
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 
-		
-		
-		
-		if (!shader_reload_failed) {
-			//
-			// Todo: Render all your geometry here.
-			//
-			skybox.render(mCamera.GetWorldToClipMatrix());
+		//mCamera.Update(deltaTimeUs, inputHandler);
+		switch (gameState)
+		{
+		case GameState::GAME_START:
+			ImGui::SetNextWindowPos(ImVec2(config::resolution_x * 0.5f, config::resolution_y * 0.5f), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
+			ImGui::SetNextWindowSize(ImVec2(config::resolution_x * 0.23f, config::resolution_y * 0.3f), ImGuiCond_Always);
 
-			///render the spaceship
-			//Render mesh by mesh
-			
-			for (unsigned int i = 0; i < spaceShipObj.size(); i++)
-			{
-				spaceship.set_geometry(spaceShipObj[i]);
-				if ("Low_Vehicle_Circle.014" == spaceShipObj[i].name)
+			ImGui::Begin("LOST IN THE UNIVERSE", nullptr, ImGuiWindowFlags_None);
+			ImGui::Text(" ");
+			ImGui::Text("		Try to go as far as you can.	   ");
+			ImGui::Text("Manover to avoid colision with the asteroids");
+			ImGui::Text("		    Shoot to get scores				");
+			ImGui::Text(" ");
+			ImGui::Separator();
+			ImGui::Text("CONTROLS: ");
+			ImGui::Text("	- MANOEUVER:     	W,S,A and D keys");
+			ImGui::Text("	- LOOK AROUND:  	Mouse");
+			ImGui::Text("	- SHOOT: 			Left mouse button");
+			ImGui::Text(" ");
+			ImGui::Separator();
+			ImGui::Text(" ");
+			if (ImGui::Button("START", ImVec2(ImGui::GetWindowSize().x * 0.98f, 0.0f)))
+				gameState = GameState::GAME_RUN;
+			ImGui::End();
+			break;
+		case GameState::GAME_RUN:
+
+			//Control the spaceship
+			if (inputHandler.GetKeycodeState(GLFW_KEY_A) & PRESSED) {
+				spaceshipUpdate = true;
+				spaceshipLocation = -spaceshipHorSpeed;
+			}
+
+			if (inputHandler.GetKeycodeState(GLFW_KEY_D) & PRESSED) {
+				spaceshipUpdate = true;
+				spaceshipLocation = spaceshipHorSpeed;
+			}
+
+			if (inputHandler.GetKeycodeState(GLFW_KEY_W) & PRESSED) {
+				spaceshipVerSpeed = 80.0f;
+			}
+
+			if (inputHandler.GetKeycodeState(GLFW_KEY_S) & PRESSED) {
+				spaceshipVerSpeed = -20.0f;
+			}
+
+			if (inputHandler.GetKeycodeState(GLFW_KEY_W) & JUST_RELEASED) {
+				spaceshipVerSpeed = 0.0f;
+			}
+
+			if (inputHandler.GetKeycodeState(GLFW_KEY_S) & JUST_RELEASED) {
+				spaceshipVerSpeed = 0.0f;
+			}
+
+			if (!shader_reload_failed) {
+				//
+				// Todo: Render all your geometry here.
+				//
+				skybox.render(mCamera.GetWorldToClipMatrix());
+
+				///render the spaceship
+				//Render mesh by mesh
+
+				for (unsigned int i = 0; i < spaceShipObj.size(); i++)
 				{
-					meshID = 0;
+					spaceship.set_geometry(spaceShipObj[i]);
+					if ("Low_Vehicle_Circle.014" == spaceShipObj[i].name)
+					{
+						meshID = 0;
+					}
+					else if ("Low_Engine_Circle.030" == spaceShipObj[i].name)
+					{
+						meshID = 1;
+					}
+					else
+					{
+						meshID = 2;
+					}
+					if (true == spaceshipUpdate)
+					{
+						spaceshipTransform = glm::translate(spaceshipTransform, glm::vec3(spaceshipLocation, 0.0f, 0.0f));
+						spaceshipUpdate = false;
+					}
+					spaceship.render(mCamera.GetWorldToClipMatrix(), spaceshipTransform);
 				}
-				else if ("Low_Engine_Circle.030" == spaceShipObj[i].name)
-				{
-					meshID = 1;
-				}
-				else
-				{
-					meshID = 2;
-				}
-				if (true == spaceshipUpdate)
-				{
-					spaceshipTransform = glm::translate(spaceshipTransform, glm::vec3(spaceshipLocation, 0.0f, 0.0f));
-					spaceshipUpdate = false;
-				}
-				spaceship.render(mCamera.GetWorldToClipMatrix(), spaceshipTransform);
+				glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+				spaceshipSphere.render(mCamera.GetWorldToClipMatrix(), spaceshipTransform);
+				glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
 				for (unsigned int i = 0; i < noOfAsteroids; i++)
 				{
@@ -376,9 +402,13 @@ edaf80::Assignment5::run()
 					asteroid.render(mCamera.GetWorldToClipMatrix(), asteroidTransform);
 
 				}
+
 			}
-			
+			break;
+		case GameState::GAME_FINISH:
+			break;
 		}
+
 
 
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
